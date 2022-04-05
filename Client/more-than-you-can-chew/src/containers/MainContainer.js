07600efd095b0be66054933c5be55efd1ad11430
tmcgregor1989 <tmcgregor1989@gmail.com>
2022-04-05 12:01:08 +0100

    bakery container again

--- Client/more-than-you-can-chew/src/containers/MainContainer.js
+++ Client/more-than-you-can-chew/src/containers/MainContainer.js
@@ -1,12 +1,27 @@
+import React, {Fragment, component} from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import UserContainer from './UserContainer';
+import BakeryContainer from './BakeryContainer';
+
+
+
 const MainContainer = () => {
 
 
 
     return(
-        <>
-        <p>This is the main container</p>
-        </>
-    )
+        <Router>
+        <Fragment>
+        <Routes>
+          <Route path="/users" element={<UserContainer/>}/>
+          <Route path="/bakeries" element={<BakeryContainer/>}/>
+  
+        </Routes>
+  
+  
+        </Fragment>
+        </Router>
+      )
 }
 
 export default MainContainer
