83f687bc7e7819e3cee891fc01c28d397dbb1846
Christopher Mckenna <christopher.mckenna123@gmail.com>
2022-04-05 16:16:35 +0100

    bakerybyid

--- Client/more-than-you-can-chew/src/components/bakeries/BakeryNewExisting.js
+++ Client/more-than-you-can-chew/src/components/bakeries/BakeryNewExisting.js
@@ -2,23 +2,26 @@ import React from "react";
 import {Link} from 'react-router-dom';
 
 
+
 const BakeryNewExisting = () => {
 
     // look at pirate form 
     
     const handleSubmit = () => {
-        return null
+        return window.location.href='/id';
     }
 
     const handleBakery= () => {
+        
         return null
     }
 
     return (
         <div className="BakeryNewExisting">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={<Link to={"/id"}/>}>
             <select name="bakery" onchange={handleBakery} defaultValue="select-bakery">
                 <option disabled value="select-bakery">Select Bakery</option>
+                <option value="test">Test</option>
                 {/* put in list of bakeries */}
             </select>
             <button type="submit">Login</button>
