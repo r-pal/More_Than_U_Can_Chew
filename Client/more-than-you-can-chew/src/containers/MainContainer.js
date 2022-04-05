b99bae0597108c636f4501fa136dcaac5baac61c
tmcgregor1989 <tmcgregor1989@gmail.com>
2022-04-05 14:49:08 +0100

    new bakery form component

--- Client/more-than-you-can-chew/src/containers/MainContainer.js
+++ Client/more-than-you-can-chew/src/containers/MainContainer.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
 import UserContainer from './UserContainer';
 import BakeryContainer from './BakeryContainer';
 import HomePage from '../components/HomePage';
+import NewBakerForm from '../components/bakeries/NewBakerForm';
 
 
 
@@ -18,6 +19,7 @@ const MainContainer = () => {
             <Route path="/" element={<HomePage/>}/>
           <Route path="/users" element={<UserContainer/>}/>
           <Route path="/bakeries" element={<BakeryContainer/>}/>
+          <Route path="bakeries/new" element={<NewBakerForm/>}/>
 
   
         </Routes>
