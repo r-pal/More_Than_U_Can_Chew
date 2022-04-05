6e8afb9ede7aabc186bde9cf72a1661a8e8a1796
Mattbusby <95364588+Mattbusby@users.noreply.github.com>
2022-04-05 10:27:44 +0100

    Spring framework files added

---
+++ src/test/java/com/codeclan/example/More_Than_U_Can_Chew/MoreThanUCanChewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.More_Than_U_Can_Chew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoreThanUCanChewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
