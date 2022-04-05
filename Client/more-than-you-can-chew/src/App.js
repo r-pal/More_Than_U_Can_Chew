fc0d7d1cc69ad3fe717da82337169b5ef1e0ce63
tmcgregor1989 <tmcgregor1989@gmail.com>
2022-04-05 10:57:18 +0100

    mainContainer

--- Client/more-than-you-can-chew/src/App.js
+++ Client/more-than-you-can-chew/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
+import MainContainer from './containers/MainContainer';
+
+class App extends Component {
+
+  render() {
+
   return (
-    <>
-    <p>Hi</p>
-    </>
+    <div>
+      <MainContainer/>
+    </div>
+        
   );
+  }
 }
 
 export default App;
