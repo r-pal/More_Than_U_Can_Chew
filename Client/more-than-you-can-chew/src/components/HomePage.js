24a71d5a02d66b320716d52f74de03b21eb7efee
Christopher Mckenna <christopher.mckenna123@gmail.com>
2022-04-07 13:14:11 +0100

    css start

--- Client/more-than-you-can-chew/src/components/HomePage.js
+++ Client/more-than-you-can-chew/src/components/HomePage.js
@@ -1,13 +1,18 @@
 import React, {Fragment, component} from 'react';
 import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+import "./stylesheets/HomePage.css";
 
 const HomePage = () => {
 
 
     return(
-        <div className="buttonContainer">
+        <div className='HomePageContainer'>
+        <div className="ButtonUsers">
         <Link to={"/users"}><button type="button">Sign in as User</button></Link>
+        </div>
+        <div className='ButtonBakeries'>
         <Link to={"bakeries"}><button type="button">Sign in as Baker</button></Link>
+</div>
 </div>
     )
 }
