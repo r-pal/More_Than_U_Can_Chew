20f3b7e8f6ed7c1350135f8a63df00ba6fd0509b
tmcgregor1989 <tmcgregor1989@gmail.com>
2022-04-12 11:03:38 +0100

    fixed

--- Client/more-than-you-can-chew/src/components/users/BakeryListItem.js
+++ Client/more-than-you-can-chew/src/components/users/BakeryListItem.js
@@ -12,16 +12,19 @@ const BakeryListItem = ({bakery, selectedUser, setSelectedBakery, images}) => {
 
 
   return (
-    <Link onClick={handleClick} className="navBarLink" style={{textDecoration:"none"}} to={"bakery"}><li className="bakeriesNearContainer">
+    <>
+    <Link onClick={handleClick} className="navBarLink" style={{textDecoration:"none"}} to={"bakery"}>
+      <li className="bakeriesNearContainer">
         <h2>{bakery.name}</h2>
       <h3>Location: {bakery.location}</h3>
       <h3>email: {bakery.email}</h3>
       <h3>Collection Time: {bakery.collectionTime}</h3>
-
+      </li></Link>
       <div className="App">
       <MapBox /> 
     </div>
-    </li></Link>
+    
+    </>
 
   )
 }
