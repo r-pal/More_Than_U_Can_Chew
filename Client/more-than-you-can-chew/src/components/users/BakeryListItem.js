625be3adabdb8a02ab1331b16ecc6f759dd918f7
Christopher Mckenna <christopher.mckenna123@gmail.com>
2022-04-12 12:54:13 +0100

    geo fetch from matt

--- Client/more-than-you-can-chew/src/components/users/BakeryListItem.js
+++ Client/more-than-you-can-chew/src/components/users/BakeryListItem.js
@@ -21,7 +21,7 @@ const BakeryListItem = ({bakery, selectedUser, setSelectedBakery, images}) => {
       <h3>Collection Time: {bakery.collectionTime}</h3>
       </li></Link>
       <div className="App">
-      <MapBox /> 
+      <MapBox bakery={bakery}/> 
     </div>
     
     </>
